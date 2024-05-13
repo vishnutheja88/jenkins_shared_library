13ec81c1d71d5789198c1b76985a5acb6173eb63
vishnutheja88 <vishnutheja88@gmail.com>
2024-05-13 10:53:37 -0400

    Create dockerbuild.groovy

---
+++ vars/dockerbuild.groovy
@@ -0,0 +1,12 @@
+def call(String project) {
+
+
+
+        
+  sh 'docker build . -t deekshithsn/devops-training:$Docker_tag'
+		   withCredentials([string(credentialsId: 'docker_password', variable: 'docker_password')]) {
+				    
+				  sh 'docker login -u deekshithsn -p $docker_password'
+				  sh 'docker push deekshithsn/devops-training:$Docker_tag'
+			}
+}
